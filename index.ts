bc9ade7e3afd9bd4bb212053d99607164d15ed56
Azeem000011111111 <139073260+Azeem000011111111@users.noreply.github.com>
2023-07-15 09:27:10 +0500

    .

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("hello")
+var output:Array <string|number> = [];
+var count = 1;
+function fizzbuzz(){
+    for(var count = 1; count<=101;count++){
+        if (count %3 ===0 && count%5===0){
+            output.push("fizzbuzz")
+        } 
+        else if(count%3 ===0){
+            output.push("fizz")
+        }
+        else if(count % 5 ===0){
+            output.push("buzz")
+        }
+        else{
+            output.push(count)
+        }
+    }
+    console.log(output)
+}
+fizzbuzz()
